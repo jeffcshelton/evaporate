2e6effc069b4c888d606d0bc5332c30a106c74d4
Jeffrey Shelton <jeffrey.shelton.c@gmail.com>
2022-05-18 01:15:31 -0400

    Write Manifest struct for interacting with manifest db

---
+++ src/manifest.rs
@@ -0,0 +1,30 @@
+use rusqlite::{Connection as DbConnection, params, Result};
+use std::path::{Path, PathBuf};
+
+pub struct Manifest {
+	local_backup_path: PathBuf,
+	connection: DbConnection,
+}
+
+impl Manifest {
+	pub fn open(backup_path: &Path) -> Result<Self> {
+		let manifest_path = backup_path.join("Manifest.db");
+
+		Ok(Self {
+			local_backup_path: backup_path.to_owned(),
+			connection: DbConnection::open(manifest_path)?,
+		})
+	}
+
+	pub fn get_path(&self, device_relative_path: &str) -> Result<PathBuf> {
+		let mut sql = self.connection.prepare("SELECT fileID FROM Files WHERE relativePath=?1")?;
+		let mut rows = sql.query(params![device_relative_path])?;
+
+		let file_hash: String = rows.next()?.unwrap().get(0)?; // TODO: Internal, but remove .unwrap()
+		let full_path = self.local_backup_path
+			.join(&file_hash[..2])
+			.join(file_hash);
+
+		Ok(full_path)
+	}
+}
