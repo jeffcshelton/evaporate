4069e7c6c9b95a90d035a25617b41ef756b300ea
Jeffrey Shelton <jeffrey.shelton.c@gmail.com>
2022-05-18 01:34:04 -0400

    Tweak get_phone_number fn

--- src/address_book.rs
+++ src/address_book.rs
@@ -14,18 +14,18 @@ impl AddressBook {
 		})
 	}
 
-	fn get_phone_number(&self, name: &str) -> Result<String> {
+	pub fn get_phone_number(&self, name: &str) -> Result<String> {
 		let name = name.split(' ').collect::<Vec<&str>>();
 		let first_name = name.first();
 		let last_name = name.last();
 
 		let mut person_sql = self.connection.prepare("SELECT RowID FROM ABPerson WHERE First=?1 AND Last=?2")?;
 		let mut person_rows = person_sql.query(params![first_name, last_name])?;
-		let person_id: i32 = person_rows.next()?.unwrap().get(0)?; // TODO: Internal, but remove .unwrap()
+		let person_id: i32 = person_rows.next()?.unwrap().get(0)?; // TODO: Remove .unwrap()
 
-		let mut number_sql = self.connection.prepare("SELECT Value FROM ABMultiValue WHERE record_id=?1 AND property=?2")?;
+		let mut number_sql = self.connection.prepare("SELECT value FROM ABMultiValue WHERE record_id=?1 AND property=?2")?;
 		let mut number_rows = number_sql.query(params![person_id, 3_i32])?;
-		let phone_number: String = number_rows.next()?.unwrap().get(0)?; // TODO: Internal, but remove .unwrap()
+		let phone_number: String = number_rows.next()?.unwrap().get(0)?; // TODO: Remove .unwrap()
 
 		Ok(phone_number)
 	}
