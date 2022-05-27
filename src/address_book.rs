70075f7e8e292f39885e3c9ef33d28f3ddd34000
Jeffrey Shelton <jeffrey.shelton.c@gmail.com>
2022-05-26 22:13:14 -0400

    Fix person-fetching & format phone numbers

--- src/address_book.rs
+++ src/address_book.rs
@@ -6,17 +6,26 @@ pub struct AddressBook {
 
 impl AddressBook {
 	pub fn get_phone_number(&self, name: &str) -> Result<String> {
-		let name = name.split(' ').collect::<Vec<&str>>();
-		let first_name = name.first();
-		let last_name = name.last();
-
-		let mut person_sql = self.connection.prepare("SELECT RowID FROM ABPerson WHERE First=?1 AND Last=?2")?;
-		let mut person_rows = person_sql.query(params![first_name, last_name])?;
-		let person_id: i32 = person_rows.next()?.unwrap().get(0)?; // TODO: Remove .unwrap()
+		let mut person_sql = self.connection.prepare("SELECT RowID FROM ABPerson WHERE (First || ' ' || Last)=?1")?;
+		let mut person_rows = person_sql.query(params![name])?;
+		let person_id = person_rows 
+			.next()?
+			.unwrap() // TODO: Remove .unwrap()
+			.get::<_, i32>(0)?;
 
 		let mut number_sql = self.connection.prepare("SELECT value FROM ABMultiValue WHERE record_id=?1 AND property=?2")?;
 		let mut number_rows = number_sql.query(params![person_id, 3_i32])?;
-		let phone_number: String = number_rows.next()?.unwrap().get(0)?; // TODO: Remove .unwrap()
+		let mut phone_number = number_rows
+			.next()?
+			.unwrap() // TODO: Remove .unwrap()
+			.get::<_, String>(0)?
+			.replace(['(', ')', ' ', '-'], "");
+		
+		if !phone_number.starts_with('+') {
+			phone_number = "+1".to_owned() + &phone_number;
+		}
+
+		println!("{}", &phone_number);
 
 		Ok(phone_number)
 	}
