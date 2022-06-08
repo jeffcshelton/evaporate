5e9bd1b93d3f0728fb109d0d09b3ed875fc54834
Jeffrey Shelton <jeffrey.shelton.c@gmail.com>
2022-06-07 23:40:44 -0400

    Replace Messages.extract with fetch fn

--- src/messages.rs
+++ src/messages.rs
@@ -1,61 +1,61 @@
-use chrono::{Local, NaiveDateTime, TimeZone};
-use crate::{address_book::Contact, Result, TIMESTAMP_OFFSET, DATE_FORMAT_STR};
+use chrono::{Local, NaiveDateTime, TimeZone, DateTime};
 use rusqlite::{Connection as DbConnection, params};
-use std::{fs::{self, File}, io::Write, path::Path};
+use std::collections::HashMap;
+
+use crate::{
+	address_book::Contacts,
+	manifest::Manifest,
+	Result,
+	TIMESTAMP_OFFSET,
+};
+
+pub struct Message {
+	content: Option<String>,
+	is_from_me: bool,
+	timestamp: DateTime<Local>
+}
 
 pub struct Messages {
-	pub(crate) connection: DbConnection
+	messages: HashMap<String, Vec<Message>>
 }
 
 impl Messages {
-	pub fn extract<P: AsRef<Path>>(&self, contact: &Contact, path: P) -> Result<()> {
-		let mut file = File::create(path.as_ref())?;
+	pub fn fetch(manifest: &Manifest, contacts: &Contacts) -> Result<Self> {
+		let connection = DbConnection::open(manifest.get_path("Library/SMS/sms.db")?)?;
+		let mut sql = connection.prepare("
+				SELECT
+					text,
+					is_from_me,
+					date / 1000000000
+				FROM message
+				WHERE handle_id=(
+					SELECT RowID
+					FROM handle
+					WHERE id=?1 AND service IS NOT NULL
+				) AND type=?2
+			")?;
 
-		let mut sql = self.connection.prepare("
-			SELECT text, is_from_me, date
-			FROM message
-			WHERE handle_id=(
-				SELECT RowID
-				FROM handle
-				WHERE id=?1 AND service=?2
-			) AND type=?3
-		")?;
+		let mut messages = HashMap::new();
 
-		let mut rows = sql.query(params![contact.phone_number, "iMessage", 0_i32])?;
-		let mut last_timestamp = 0;
+		for contact in contacts.iter() {
+			let mut rows = sql.query(params![contact.phone_number, 0_i32])?;
+			let mut conversation: Vec<Message> = Vec::new();
 
-		let mut is_empty = true;
+			while let Some(row) = rows.next()? {
+				let timestamp = Local.from_utc_datetime(
+					&NaiveDateTime::from_timestamp(row.get::<_, i64>(2)? + TIMESTAMP_OFFSET, 0)
+				);
 
-		while let Some(row) = rows.next()? {
-			let content = row.get::<_, Option<String>>(0)?;
-			let is_from_me = row.get::<_, i32>(1)? == 1;
-			let raw_timestamp = row.get::<_, i64>(2)? / 1_000_000_000;
-
-			if raw_timestamp - last_timestamp > 7200 { // difference of 2 hours
-				let datetime = Local.from_utc_datetime(&NaiveDateTime::from_timestamp(raw_timestamp + TIMESTAMP_OFFSET, 0));
-
-				file.write_all(
-					format!("\n      | {} |\n\n", datetime.format(DATE_FORMAT_STR))
-						.as_bytes()
-				)?;
+				conversation.push(Message {
+					content: row.get::<_, Option<String>>(0)?,
+					is_from_me: row.get::<_, bool>(1)?,
+					timestamp: timestamp,
+				});
 			}
 
-			file.write_all(
-				format!("[{}]: {}\n",
-					if is_from_me { "me" } else { &contact.first_name },
-					content.clone().unwrap_or("<unknown>".to_owned()),
-				).as_bytes(),
-			)?;
-
-			last_timestamp = raw_timestamp;
-			is_empty = false;
+			messages.insert(contact.name(), conversation);
 		}
 
-		if is_empty {
-			drop(file);
-			fs::remove_file(path.as_ref())?;
-		}
-
-		Ok(())
+		Ok(Self { messages: messages })
 	}
 }
