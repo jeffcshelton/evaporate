8c79e23e33bfe494371662c7ad1beaf4d98cc5c4
Jeffrey Shelton <jeffrey.shelton.c@gmail.com>
2022-06-09 16:41:28 -0400

    Rewrite Messages SQL to isolate it from contacts

--- src/messages.rs
+++ src/messages.rs
@@ -9,7 +9,6 @@ use chrono::{
 };
 
 use crate::{
-	contacts::Contacts,
 	constants::{
 		DATETIME_FORMAT_STR,
 		TIMESTAMP_OFFSET,
@@ -36,40 +35,59 @@ pub struct Messages {
 }
 
 impl Messages {
-	pub fn fetch(manifest: &Manifest, contacts: &Contacts) -> Result<Self> {
-		let connection = DbConnection::open(manifest.get_path("Library/SMS/sms.db")?)?;
+	pub fn fetch(manifest: &Manifest) -> Result<Self> {
+		let messages_path = manifest.get_path("Library/SMS/sms.db")?;
+		let contacts_path = manifest.get_path("Library/AddressBook/AddressBook.sqlitedb")?;
+
+		let connection = DbConnection::open(messages_path)?;
+		connection.execute("ATTACH DATABASE ?1 AS AddressBook", params![contacts_path.to_string_lossy()])?;
+
 		let mut sql = connection.prepare("
-				SELECT
-					text,
-					is_from_me,
-					date / 1000000000
-				FROM message
-				WHERE handle_id=(
-					SELECT RowID
-					FROM handle
-					WHERE id=?1 AND service IS NOT NULL
-				) AND type=?2
-			")?;
+			SELECT
+				Message.text,
+				Message.is_from_me,
+				Message.date / 1000000000,
+				Contact.First || COALESCE(' ' || Contact.Last, '')
+			FROM Message
+			INNER JOIN AddressBook.ABMultiValue AS PhoneNumber ON
+				PhoneNumber.property = 3
+				AND handle.id = REPLACE(REPLACE(REPLACE(REPLACE(
+					CASE WHEN PhoneNumber.value NOT LIKE '+%'
+						THEN '+1' || PhoneNumber.value
+						ELSE PhoneNumber.value
+					END,
+				'(', ''), ')', ''), ' ', ''), '-', '')
+			INNER JOIN AddressBook.ABPerson AS Contact ON
+				Contact.RowID = PhoneNumber.record_id
+			INNER JOIN handle ON
+				Message.handle_id=handle.RowID
+				AND handle.service IS NOT NULL
+			WHERE
+				Message.type = 0
+				AND Contact.First IS NOT NULL
+			ORDER BY Message.date ASC
+		")?;
 
-		let mut messages = HashMap::new();
+		let mut messages: HashMap<String, Vec<Message>> = HashMap::new();
+		let mut rows = sql.query([])?;
 
-		for contact in contacts.iter() {
-			let mut rows = sql.query(params![contact.phone_number, 0_i32])?;
-			let mut conversation: Vec<Message> = Vec::new();
+		while let Some(row) = rows.next()? {
+			let name: String = row.get(3)?;
+			let timestamp = Local.from_utc_datetime(
+				&NaiveDateTime::from_timestamp(row.get::<_, i64>(2)? + TIMESTAMP_OFFSET, 0)
+			);
 
-			while let Some(row) = rows.next()? {
-				let timestamp = Local.from_utc_datetime(
-					&NaiveDateTime::from_timestamp(row.get::<_, i64>(2)? + TIMESTAMP_OFFSET, 0)
-				);
+			let message = Message {
+				content: row.get(0)?,
+				is_from_me: row.get(1)?,
+				timestamp: timestamp,
+			};
 
-				conversation.push(Message {
-					content: row.get::<_, Option<String>>(0)?,
-					is_from_me: row.get::<_, bool>(1)?,
-					timestamp: timestamp,
-				});
+			if let Some(conversation) = messages.get_mut(&name) {
+				conversation.push(message);
+			} else {
+				messages.insert(name, vec![message]);
 			}
-
-			messages.insert(contact.name(), conversation);
 		}
 
 		Ok(Self { messages: messages })
